b70e19ee3bf7ef9fafe0e0507da6d15fcfeba155
guilhermeabel99 <gpeuvis@gmail.com>
2020-09-05 16:28:23 -0300

    Add: components to show the first user in the rank

--- src/Pages/private_access/rank/index.tsx
+++ src/Pages/private_access/rank/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import HeaderComponent from '../../../Components/HeaderPrivateComponent/index'
+import UserSectionComponent from './components/UserSectionComponent/index'
 
-const PublicHomePage = () => {
+import UserRank from '../rank/components/UserRankComponent'
+
+const PrivateRankPage = () => {
     return (
-        <div id="homePublic-page">
-            <div className="homePublic-content">
-                <p className="homePublic-text">Home Page publica!</p>
+        <div id="rankPrivate-page">
+            <div className="rankPrivate-content" > 
+                <HeaderComponent />
+                <UserSectionComponent />
+            </div>
+
+            <UserRank />
+            <div>                
+                <p></p>
+                <span className="separator-rank"></span>
             </div>
         </div>
     )
 }
 
-export default PublicHomePage
+export default PrivateRankPage
