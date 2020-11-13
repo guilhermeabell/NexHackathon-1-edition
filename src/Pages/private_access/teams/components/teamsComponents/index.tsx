d72ce18dd449863d73fc0834f4a4b5dceb85e127
Pedro Augusto <p.augusto0210@gmail.com>
2020-11-13 16:53:07 -0200

    FIX: get team endpoint

--- src/Pages/private_access/teams/components/teamsComponents/index.tsx
+++ src/Pages/private_access/teams/components/teamsComponents/index.tsx
@@ -18,7 +18,18 @@ const TeamsPrivate = () => {
 
     const [modal, setModal] = useState(false)
 
+    const [team, setTeam] = useState('')
+
     function openModal(id_teams) {
+
+        api.get('/team').then((response) => {
+            if(response && response.data) {
+                setTeam(response.data)
+            }
+        })
+
+        console.log(team)
+
         setModal(true);
     }
 
@@ -28,13 +39,13 @@ const TeamsPrivate = () => {
 
     const [teams, setTeams] = useState<Teams[]>([])
 
-    // useEffect(() => {
-    //     api.get('/teams').then((response) => {
-    //         if(response && response.data) {
-    //             setTeams(response.data)
-    //         }
-    //     })
-    // })
+    useEffect(() => {
+        api.get('/teams').then((response) => {
+            if(response && response.data) {
+                setTeams(response.data)
+            }
+        })
+    })
 
     return (
         <div className="teamsPrivate-main">
@@ -85,7 +96,7 @@ const TeamsPrivate = () => {
                         {teams.map((team, index) => (
                         <div className="teamsPrivate-main-card" onClick={() => openModal(team.id_teams)} key={team.id_teams}>
                             <div className="teamsPrivate-item-header">
-                                <p className='teamsPrivate-item-title'>Time {team.title}</p>
+                                <p className='teamsPrivate-item-title'>Time {team.id_teams}</p>
                                 <div className="card-header-count">
                                     <FaRegUser size='25' color='#f4f4f4'/>
                                     <p className='teamsPrivate-item-count'>2/5</p>
@@ -93,15 +104,6 @@ const TeamsPrivate = () => {
                             </div>
                         </div>
                         ))}
-
-                        <div className="teamsPrivate-main-card" onClick={openModal}>
-                            <p className='teamsPrivate-item-title'>Time 1</p>
-                            <div className="card-header-count">
-                                <FaRegUser size='25' color='#f4f4f4'/>
-                                <p className='teamsPrivate-item-count'>2/5</p>
-                            </div>
-                        </div>
-
                     </div>
             </div>
         </div>
