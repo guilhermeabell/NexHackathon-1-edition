29aec8d1d229ac5e805f3d564ea4baf48029b601
Pedro Augusto <p.augusto0210@gmail.com>
2020-11-21 12:44:09 -0200

    New feature

--- src/Pages/public_access/home_public/components/InfoSection/index.tsx
+++ src/Pages/public_access/home_public/components/InfoSection/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
-// Image
-import Illustration from '../../assets/Home/homeIllustration.svg'
-
 import './styles.css'
 
 const InfoSectionComponent = () => {
     return (
         <div className="homePublic-section1">
             <div className="section1-content">
-                <img src={Illustration} />
+                <div className="section1-title">
+                    <h1>
+                        <span>Criatividade.</span>
+                        <span>Conhecimento.</span>
+                        <span>Carreira.</span>
+                    </h1>
+                </div>
 
                 <div className="section1-info">
                     <div className="section1-info-top">
@@ -18,7 +21,7 @@ const InfoSectionComponent = () => {
                     </div>
 
                     <div className="section1-info-bottom">
-                        <p className="section1-title">O que é um Hackathon?</p>
+                        <p className="section1-info-title">O que é um Hackathon?</p>
                         <p className="section1-description">Hackathons são maratonas que envolvem pessoas interessadas em solucionar problemas e desenvolver soluções inovadoras para questões específicas.</p>
                     </div>
                 </div>
